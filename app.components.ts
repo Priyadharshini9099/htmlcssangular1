9d0f24223f723c8808ff06352511c9767c9314eb
Priyadharshini9099 <85815948+Priyadharshini9099@users.noreply.github.com>
2021-06-17 19:53:55 +0530

    Add files via upload

---
+++ app.components.ts
@@ -0,0 +1,11 @@
+import {component} from '@angular/core';
+@component({
+selector:'app-root',
+template:
+<div style="text-align:center">
+<h1>Play</h1>
+styles:[]
+})
+export class App Component{
+   title = 'app';
+}
---
+++ home.components.ts
@@ -0,0 +1,22 @@
+import{component,OnInit} from '@angular/core';
+@selector:'app - home',
+template:
+<section class = "hero-is-primary-is-bold-is full height">
+<div class="hero-body">
+<div class="container has text-centered">
+<h1 class= "title"> GoGamesLive</h1>
+</div>
+</div>
+</section>
+styles:['.hero{
+background-image: url('/assets/img/green.jpeg)! important;
+background-size:cover;
+background-position:center;
+}
+}
+})
+export class HomeComponent implements OnInit
+constructor(){}
+ngOnInit(){
+}
+}	
